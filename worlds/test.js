44498bde38e782abef4a706b481548f31f149c8d
drwh <thura@drwh.net>
2018-12-19 18:04:54 +0000

    update

--- worlds/test.js
+++ worlds/test.js
@@ -1,4 +1,4 @@
-const scenes = [
+const rooms = [
   {
   name:"Start",
   desc:"Here is the entrance",
@@ -8,8 +8,23 @@ const scenes = [
   name:"Inside the Maze",
   desc:"You are at the begining of the maze and here you see a fountain",
   loc:{x:0,y:1}
+},
+{
+  name:"North Passage",
+  desc:"There is an exit to the East",
+  loc:{x:0,y:2}
+},
+{
+  name:"Water Shrine",
+  desc:"There is a magic in the air.",
+  loc:{x:1,y:2}
+},
+{
+  name:"The Beach",
+  desc:"You see a naked Fox on the sand taking in the rays",
+  loc:"x:2,y:0"
 }
 ]
 module.exports = {
-  scenes
+  rooms
 }
