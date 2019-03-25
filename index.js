1269e729656203b048b5fcb934d9536b5f899a14
drwh <thura@drwh.net>
2019-03-25 19:31:43 +0000

    print state

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const world = require("./worlds/test")
 const game = require('./game')
 const print = require("./print").cli
-const state = game.initialState(world)
 
-console.log(state)
+const state = game.initialState(world)
 
 print.printState(state)
 //console.log(game.initialState(world)
